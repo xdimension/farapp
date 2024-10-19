73d89150effa09c46b2d9ad5dad9d1b16941a762
xdimension <xdim222@gmail.com>
2024-10-19 23:06:57 +0700

    rename var

--- inertia/pages/admin/coupons/list.tsx
+++ inertia/pages/admin/coupons/list.tsx
@@ -4,20 +4,20 @@ import useModal from '~/hooks/useModal'
 import LayoutAdmin from '~/layouts/LayoutAdmin'
 
 function List() {
-  const modelCreateCoupon = useModal()
+  const modalCreateCoupon = useModal()
 
   return (
     <LayoutAdmin>
 
       <div className="text-end">
         <button
-          onClick={modelCreateCoupon.openModal}
+          onClick={modalCreateCoupon.openModal}
           className="bg-blue-500 px-3 py-2 rounded-md text-white"
         >
           Create
         </button>
       </div>
-      {modelCreateCoupon.isOpen && <ModalCreateCoupon close={modelCreateCoupon.closeModal} />}
+      {modalCreateCoupon.isOpen && <ModalCreateCoupon close={modalCreateCoupon.closeModal} />}
       <Coupons />
 
     </LayoutAdmin>
