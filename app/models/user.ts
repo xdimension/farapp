591b38c29c3a56dfec12ada81ef55307f55036d0
xdimension <xdim222@gmail.com>
2024-10-24 15:08:03 +0000

    add role relation in user model

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,7 +1,9 @@
 import { DateTime } from 'luxon'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import Role from './role.js'
+import { BaseModel, column, belongsTo } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
 import { DbRememberMeTokensProvider } from '@adonisjs/auth/session'
 
@@ -22,6 +24,7 @@ export default class User extends compose(BaseModel, AuthFinder) {
 
   @column({ serializeAs: null })
   declare password: string
+
   @column()
   declare roleId: number
 
@@ -31,6 +34,8 @@ export default class User extends compose(BaseModel, AuthFinder) {
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime | null
 
+  @belongsTo(() => Role)
+  declare role: BelongsTo<typeof Role>
 
   static rememberMeTokens = DbRememberMeTokensProvider.forModel(User)
 }
